de788d5ddfdfdb28e30f2134889a0e17deed5908
Mahidul Islam <86832773+codermahidul@users.noreply.github.com>
2021-07-08 17:32:16 +0600

    Redo Menu and banner add

---
+++ js/script.js
@@ -0,0 +1,35 @@
+$(function(){
+    'use strict';
+    
+    // Type Js
+    var typed = new Typed('.type', {
+        strings: ['Web Designer', 
+        'Web Devoloper',
+        'SEO Expert',
+        'WP Theme Devoloper'
+    ],
+        loop: true,
+        typeSpeed: 60,
+        backSpeed: 60,
+      });
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+});
